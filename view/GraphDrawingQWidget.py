8913c06332e339bdd2c101e2e5931ed6def7aef9
Niklas Ueter <niklas.ueter@tu-dortmund.de>
2019-07-13 16:35:01 +0200

    DAG Drawing Test

--- view/GraphDrawingQWidget.py
+++ view/GraphDrawingQWidget.py
@@ -3,11 +3,29 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 import matplotlib.pyplot as plt
 import random
-
+import networkx as nx
 
 class GraphDrawingQWidget(QWidget):
     def __init__(self, parent=None):
         super(QWidget, self).__init__(parent)
+
+        G = nx.DiGraph()
+
+        # add 5 nodes, labeled 0-4:
+        map(G.add_node, range(5))
+        # 1,2 depend on 0:
+        G.add_edge(0, 1)
+        G.add_edge(0, 2)
+        # 3 depends on 1,2
+        G.add_edge(1, 3)
+        G.add_edge(2, 3)
+        # 4 depends on 1
+        G.add_edge(1, 4)
+
+        # now draw the graph:
+        pos = {0: (0, 0), 1: (1, 1), 2: (-1, 1),
+               3: (0, 2), 4: (2, 2)}
+        nx.draw(G, pos, edge_color='r')
 
         # a figure instance to plot on
         self.figure = plt.figure()
